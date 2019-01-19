e843f8863a3ee20de58847658efccf6045b0aab4
lance.erickson <lance.erickson@wheniwork.com>
2019-01-19 17:05:47 -0600

    Adding some deps

---
+++ src/components/Unstuck.vue
@@ -0,0 +1,3 @@
+<template lang="pug">
+  h1 Get Unstuck
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <Unstuck />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Unstuck from '@/components/Unstuck.vue'
+
+export default {
+  name: 'home',
+  components: {
+    Unstuck
+  }
+}
+</script>
