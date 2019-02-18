2df35c51a8d46e891c62aee262c0853aca9f037c
lance.erickson <lance.erickson@wheniwork.com>
2019-02-18 14:01:38 -0600

    Working mobile navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,16 +3,33 @@
     .navbar-brand
       .navbar-item
         router-link(to="/") UnStuck
-    .navbar-menu
+      a.navbar-burger.burger(
+        v-bind:class="{ 'is-active': menuActive }"
+        v-on:click="toggleMenu"
+      )
+        span(aria-hidden="true")
+        span(aria-hidden="true")
+        span(aria-hidden="true")
+    .navbar-menu(v-bind:class="{ 'is-active': menuActive }")
+      .navbar-start
       .navbar-end
         router-link(to="/settings") Settings
 </template>
 
 <script>
+
+function initializeData() {
+  return { menuActive: false };
+}
+
+function toggleMenu() {
+  this.menuActive = ! this.menuActive;
+}
+
 export default {
   name: 'Navbar',
   components: {},
-  data: function () {return {}}
+  data: initializeData,
+  methods: {toggleMenu},
 }
 </script>
-
